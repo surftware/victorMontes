a248e94031252a5ee795011515a0746304d90d64
and <surftware@gmail.com>
2020-05-20 19:52:15 -0500

    avance cards

--- conocenos.php
+++ conocenos.php
@@ -52,36 +52,36 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-4 col-md-4">
-          <div class="blog-grid anchoCard">
+        <div class="col-lg-4 col-md-4 anchoCard">
+          <div class="blog-grid ">
             <div class="blog-grid-img position-relative"><img alt="img" src="img/cards/ab01.png"></div>
-              <h4 class=""><a href="javascript:void(0)" class="texto_cards"> Results professional report</a></h4>
-              <div class="meta mb-5">
+            <h4 class=""><a href="javascript:void(0)" class="texto_cards"> Results professional report</a></h4>
+            <div class="meta mb-5">
 
-                <p class="">
-                  <a href="#" class="texto_cards mr-5"><i class="fas fa-calendar-alt "></i> 10 Aug, 2018</a>
-                  <a href="#" class="texto_cards mr-5"><i class="fas fa-user"></i> User</a>
-                  <a href="#" class="texto_cards"> <i class="fas fa-comments"></i> 38</a>
-                </p>
+              <p class="">
+                <a href="#" class="texto_cards mr-5"><i class="fas fa-calendar-alt "></i> 10 Aug, 2018</a>
+                <a href="#" class="texto_cards mr-5"><i class="fas fa-user"></i> User</a>
+                <a href="#" class="texto_cards"> <i class="fas fa-comments"></i> 38</a>
+              </p>
 
-              </div>
-
-              <div class="tamTextCard">
-                <p>Achitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quiavolupt assit aspernatur aut
-                  odit aut fugit, sed quia consequuntur magni.
-                </p>
-              </div>
-
-
-              <input class="btn btn-primary" type="submit" value="Submit">
-              <!--<a href="javascript:void(0)" class="">Saber más.</a>-->
             </div>
+
+            <div class="tamTextCard">
+              <p>Achitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
+                quiavolupt assit aspernatur aut
+                odit aut fugit, sed quia consequuntur magni.
+              </p>
+            </div>
+
+
+            <input class="btn btn-primary" type="submit" value="Submit">
+            <!--<a href="javascript:void(0)" class="">Saber más.</a>-->
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-4">
-          <div class="blog-grid  anchoCard">
+
+        <div class="col-lg-4 col-md-4 anchoCard">
+          <div class="blog-grid">
             <div class="blog-grid-img position-relative"><img alt="img" src="img/cards/ab03.png"></div>
             <div class="blog-grid-text p-2 ">
               <h4 class=""><a href="javascript:void(0)" class="texto_cards">Results professional report</a></h4>
@@ -114,33 +114,40 @@
           </div>
         </div>
 
-        <div class="col-lg-4 col-md-4">
-          <div class="blog-grid anchoCard">
-            <div class="blog-grid-img position-relative"><img alt="img" src="img/cards/ab04.png"></div>
+        <div class="col-lg-4 col-md-4 anchoCard">
+          <div class="blog-grid">
+            <div class="blog-grid-img position-relative"><img alt="img" src="img/cards/ab03.png"></div>
             <div class="blog-grid-text p-2 ">
               <h4 class=""><a href="javascript:void(0)" class="texto_cards">Results professional report</a></h4>
               <div class="meta mb-5">
-
                 <p class="">
                   <a href="#" class="texto_cards mr-5"><i class="fas fa-calendar-alt "></i> 10 Aug, 2018</a>
                   <a href="#" class="texto_cards mr-5"><i class="fas fa-user"></i> User</a>
                   <a href="#" class="texto_cards"> <i class="fas fa-comments"></i> 38</a>
                 </p>
+
               </div>
 
               <div class="tamTextCard">
-                <p>Achitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quiavolupt assit
-                  aspernatur
+                <p>Achitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quiavolupt assit aspernatur
                   aut
                   odit aut fugit, sed quia consequuntur magni.</p>
-              </div>
-              <input class="btn btn-primary" type="submit" value="Submit">
 
-              <!--<a href="javascript:void(0)" class="">Saber más.</a>-->
+                <p>Achitecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quiavolupt assit aspernatur
+                  aut
+                  odit aut fugit, sed quia consequuntur magni.</p>
+                <div>
+
+
+                  <input class="btn btn-primary" type="submit" value="Submit">
+                  <!--<a href="javascript:void(0)" class="">Saber más.</a>-->
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
-        
+
       </div>
     </div>
   </section>
