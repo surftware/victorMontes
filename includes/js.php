058e2a4e91d3a1ca3200463e2c02d49f5897fbea
dayaxis <diaz_teran@hotmail.com>
2020-06-30 15:25:58 -0500

    validacion correcta junto con captcha funcionando, y borrado al dar click en envio exitoso gracias jose!

--- includes/js.php
+++ includes/js.php
@@ -20,6 +20,11 @@
  <!-- font awesome-->
  <script src="fontawesome/js/all.min.js"></script>
 
+ <!-- sweetalert2-->
+ <script src="js/sweetalert2.js"></script>
+ 
+ <!-- recaptcha-->
+ <script src='https://www.google.com/recaptcha/api.js'></script>
 
  <script type="text/javascript" charset="utf-8"></script>
 
