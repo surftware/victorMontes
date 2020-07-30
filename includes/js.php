f32f589f021ade534877c3e2bb5c3a96ca4fdc1e
and <surftware@gmail.com>
2020-07-30 07:28:37 -0500

    cambio de título en logos y uso de async en includes/js

--- includes/js.php
+++ includes/js.php
@@ -1,5 +1,5 @@
  <!-- jquery -->
- <script src="js/jquery.min.js"></script>
+ <script src="js/jquery.min.js" async></script>
 
   <!-- owl carousel -->
   <script src="js/owl.carousel.min.js"></script>
@@ -11,18 +11,18 @@
 
 
  <!-- wow -->
- <script src="js/wow.min.js"></script>
+ <script src="js/wow.min.js" async></script>
 
  <!-- bootstrap js-->
- <script src="js/bootstrap.min.js"></script>
+ <script src="js/bootstrap.min.js" async></script>
 
  <!-- font awesome-->
- <script src="fontawesome/js/all.min.js"></script>
+ <script src="fontawesome/js/all.min.js" async></script>
 
  <!-- sweetalert2-->
- <script src="js/sweetalert2.js"></script>
+ <script src="js/sweetalert2.js" async></script>
  
  <!-- recaptcha-->
- <script src='https://www.google.com/recaptcha/api.js'></script>
+ <script src='https://www.google.com/recaptcha/api.js' async></script>
 
  <script type="text/javascript" charset="utf-8"></script>
