b7cb95a8447c3d256559c7cc118d0af46024e026
and <surftware@gmail.com>
2020-08-04 01:00:32 -0500

    avances revisión

--- conocenos.php
+++ conocenos.php
@@ -154,23 +154,6 @@
   </div>
   <!-- Termina Logros  -->
 
-  
-  <!-- Inicia Fechas Importantes -->
-  <div class="section ptb-50" id="Despachos">
-    <div class="container ">
-      <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-12 text-center">
-          <h1 class="h1-section">Despachos Filiales</h1>
-          <p class="text-center">
-
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Termina Logros Destacados  -->
-
-
   <!-- Inicia Despachos Filiales  -->
   <div class="section ptb-50" id="Despachos">
     <div class="container ">
@@ -185,15 +168,6 @@
     </div>
   </div>
   <!-- Termina Despachos Filiales  -->
-   <!-- SCROLL TO TOP -->
-   <div class="scrollup">
-        <a href="#"><i class="fa fa-chevron-up"></i></a>
-    </div>
-   
-    <!-- SCROLL TO TOP ENDS -->
-
-
-
 
   <?php
   include_once ("includes/footer.php");
